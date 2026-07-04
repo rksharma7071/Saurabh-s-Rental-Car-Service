70429d75a5e29af9eccf5fd81d17f3244219608e
Retesh Kumar Sharma <rksharma7071@gmail.com>
2026-07-04 16:05:16 +0530

    update api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://saurabh-s-rental-car-service-7mai.vercel.app',
-        changeOrigin: true,
-      },
-    },
-  },
 })
